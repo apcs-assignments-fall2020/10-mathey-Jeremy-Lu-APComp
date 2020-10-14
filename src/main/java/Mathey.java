e0d3f08d1b3082816759ee3df0c9b0dda7069758
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-10-14 21:04:53 +0000

    Added code for mathey classwork

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,44 @@
+import java.util.Random;
 public class Mathey {
+    public static int max(int a, int b) {
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+    public static double max(double a, double b) {
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
 
+    public static int max(int a, int b, int c) {
+        return max(a, max(b,c));
+    }
+
+    public static double max(double a, double b, double c) {
+        return max(a, max(b,c));
+    }
+
+    public static int randomInteger(int a, int b) {
+        return (int) ((b - a + 1) * Math.random()) + a;
+    }
+
+    public static int randomInteger(int b) {
+        return (int) ((b - 1) * Math.random());
+    }
+    /*
+    public static void main(String[] args) {
+        System.out.println(max(3,4,5));
+        System.out.println(max(3,4));
+        System.out.println(max(3.2,4.3,5.5));
+        System.out.println(randomInteger(3,4));
+        System.out.println(randomInteger(3));
+    }
+    */
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -7,7 +7,12 @@ public class MyMain {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Mathey.max(3,4,5));
+        System.out.println(Mathey.max(3,4));
+        System.out.println(Mathey.max(3.3,2.3));
+        System.out.println(Mathey.max(3.1,4.4,5.7));
+        System.out.println(Mathey.randomInteger(3));
+        System.out.println(Mathey.randomInteger(1,100));
     }
 
 }
